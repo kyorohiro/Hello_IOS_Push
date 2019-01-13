b580cc23ae4ae79e7276b0245f727d65046dde8c
kyorohiro <kyorohiro@gmail.com>
2019-01-13 09:16:25 +0900

    ..

--- hello-push/AppDelegate.swift
+++ hello-push/AppDelegate.swift
@@ -7,40 +7,86 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        print("##>> call didFinishLaunchingWithOptions")
         // Override point for customization after application launch.
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(
+            options: [.badge, .sound, .alert],
+            completionHandler: {(granted:Bool, error:Error?)->Void in
+                if error != nil {
+                    print("##>> \(error!.localizedDescription)")
+                    return
+                }
+                
+                if granted {
+                    DispatchQueue.main.async(execute: {
+                        application.registerForRemoteNotifications()
+                    })
+                }
+            }
+        )
         return true
     }
+    public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("##>> call didRegisterForRemoteNotificationsWithDeviceToken")
+        
+        var token = String(format: "%@", deviceToken as CVarArg) as String
+        let characterSet: CharacterSet = CharacterSet.init(charactersIn: "<>")
+        token = token.trimmingCharacters(in: characterSet)
+        token = token.replacingOccurrences(of: " ", with: "")
+        
+        //本来は、APIでサーバーへ通知する
+        print("deviceToken: \(token)")
+    }
 
+    public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        print("##>> call didReceiveRemoteNotification")
+    }
+
+    public func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("##>> call didFailToRegisterForRemoteNotificationsWithError")
+        print(error)
+    }
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    // フォアグランドでもアラートを表示する
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        print("# willPresent notification")
+        completionHandler([.badge, .sound, .alert])
+    }
+    
+    // Pushをタップした時
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("# didReceive response")
+        completionHandler()
+    }
 }
+
 
